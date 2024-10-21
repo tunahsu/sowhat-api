4c6cfabaa7b6e9841520f2045b6ecf9e1f203878
Wesley Hsu <tunahsu45@gmail.com>
2024-10-21 15:47:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,4 +39,4 @@ async def search_images(name: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8777)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
